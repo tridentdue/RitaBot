7a07f5a34b186ebd3ca20ab6fe14af63c0715eeb
tridentdue <98702375+tridentdue@users.noreply.github.com>
2022-07-31 19:43:20 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,7 +2,7 @@
 // -----------------
 // Global variables
 // -----------------
-var http = require("http");
+const http = require("http");
 setInterval(function() {
     http.get("http://translato12.herokuapp.com");
 }, 300000); // every 5 minutes (300000)
